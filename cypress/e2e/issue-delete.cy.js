3141ef717c15692de7938b696d6ca779b3fb44e5
maasax <maarit.paara@gmail.com>
2024-05-29 21:47:34 +0300

    My commets about the commit

---
+++ cypress/e2e/issue-delete.cy.js
@@ -0,0 +1,47 @@
+const issueTitle = "This is an issue of type: Task.";
+describe("Issue deletion", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.contains(issueTitle).click();
+        cy.get('[data-testid="modal:issue-details"]').should("exist");
+      });
+  });
+
+  it("Should delete an issue and validate it successfully", () => {
+    //Deleting the issue
+    cy.get('[data-testid="icon:trash"]').click();
+    cy.get('[data-testid="modal:confirm"]').should("be.visible");
+    cy.get('[data-testid="modal:confirm"]').within(() => {
+      cy.contains("Delete issue").click();
+    });
+    //Asserting the issue is deleted
+    cy.get('[data-testid="modal:confirm"]').should("not.exist");
+    cy.get('[data-testid="modal:issue-details"]').should("not.exist");
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.contains(issueTitle).should("not.exist");
+      cy.get('[data-testid="list-issue"]').should("have.length", 3);
+    });
+  });
+
+  it("Should cancel issue deletion and assert the issue still exists", () => {
+    //Cancelling deletion process
+    cy.get('[data-testid="icon:trash"]').click();
+    cy.get('[data-testid="modal:confirm"]').should("be.visible");
+    cy.get('[data-testid="modal:confirm"]').within(() => {
+      cy.contains("Cancel").click();
+    });
+
+    //Asserting the cancelling deletion process was successful
+    cy.get('[data-testid="modal:confirm"]').should("not.exist");
+    cy.get('[data-testid="modal:issue-details"]').should("exist");
+    cy.get('[data-testid="icon:close"]').eq(0).click();
+    cy.get('[data-testid="board-list:backlog"]').within(() => {
+      cy.contains(issueTitle).should("exist");
+      cy.get('[data-testid="list-issue"]').should("have.length", 4);
+    });
+  });
+});
